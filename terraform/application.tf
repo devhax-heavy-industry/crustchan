d9fdc8e19dc18597a0fc6ff96ef07c87238c2abf
Chris Svajlenka <chris.svajlenka@ariessolutions.io>
2024-10-27 13:21:46 -0700

    fix(tf): fixed tf for s3 and ddb tables

--- terraform/application.tf
+++ terraform/application.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2" {
 
-  ami                         = "ami-00756a2b7a21e2bd3" # ubuntu 24.04
+  ami                         = "ami-001651dd1b19ebcb6" # ubuntu 24.04
   instance_type               = "t2.micro"
 
   subnet_id                   = aws_subnet.public_subnet.id
@@ -59,35 +59,20 @@ resource "aws_key_pair" "ec2_key_pair" {
   public_key = file("ssh-key.pub")
 }
 
-resource "aws_s3_bucket" "app_resources" {
+resource "aws_s3_bucket_website_configuration" "app_resources" {
   bucket = "crust-chan-res"
   # acl    = "public-read"
-  policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::crust-chan-res/*"
-            ]
-        }
-    ]
-}
-EOF
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
   }
 }
 
 resource "aws_dynamodb_table" "crustchan_posts" {
-  name           = "crustchan-database"
+  name           = "crustchan-posts"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "id"
   range_key = "created_at"
@@ -150,7 +135,7 @@ resource "aws_dynamodb_table" "crustchan_posts" {
 
 
 resource "aws_dynamodb_table" "crustchan_boards" {
-  name           = "crustchan-database"
+  name           = "crustchan-boards"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "id"
   range_key = "created_at"
@@ -180,7 +165,7 @@ resource "aws_dynamodb_table" "crustchan_boards" {
 
 
 resource "aws_dynamodb_table" "crustchan_admin" {
-  name           = "crustchan-database"
+  name           = "crustchan-admin"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "id"
   range_key = "created_at"
