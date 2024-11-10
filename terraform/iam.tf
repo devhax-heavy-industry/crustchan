3dc738728dd62c8054f9ebc4f462cc677599af33
Chris Svajlenka <chris.svajlenka@ariessolutions.io>
2024-11-09 20:26:49 -0800

    added an ecs exec role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -31,6 +31,9 @@ data "aws_iam_policy" "s3_read_access" {
 data "aws_iam_policy" "dynamodb_access" {
   arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaDynamoDBExecutionRole"
 }
+data "aws_iam_policy" "ecs_task_execution" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
 
 data "aws_iam_policy" "s3_put_object_access" {
   arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
@@ -72,4 +75,28 @@ resource "aws_iam_role_policy_attachment" "lambda_rekognition_service_role_polic
 resource "aws_iam_role_policy_attachment" "lambda_dynamodb_service_role_policy_attach" {
    role       = "${aws_iam_role.app_lambda_role.name}"
    policy_arn = "${data.aws_iam_policy.dynamodb_access.arn}"
+}
+
+
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "ecs_service_role_policy_attach" {
+   role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+   policy_arn = "${data.aws_iam_policy.ecs_task_execution.arn}"
 }
