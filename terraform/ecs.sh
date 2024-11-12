adafeda6c49493b66605283d9facef1c9da3f294
Chris Svajlenka <chris.svajlenka@ariessolutions.io>
2024-11-11 19:02:16 -0800

    It turns out that the newest linux 2023 AMI doesn't update their ECS agent

--- terraform/ecs.sh
+++ terraform/ecs.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 mkdir -p /etc/ecs
 echo ECS_CLUSTER=crustchan-ecs-cluster >> /etc/ecs/ecs.config
+docker stop ecs-agent
+docker rm ecs-agent
+docker pull amazon/amazon-ecs-agent:latest
+sudo service docker restart && sudo start ecs
