5c25040ab665be6a888788b793cc5ca16920b97e
Chris Svajlenka <chris.svajlenka@ariessolutions.io>
2024-12-03 11:50:23 -0800

    fix(tf): about to destroy my AWS instances

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,21 +1,21 @@
-# resource "aws_ecs_cluster" "ecs_cluster" {
-#   name = "${var.name}-ecs-cluster"
-# }
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "${var.name}-ecs-cluster"
+}
 
-# resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
-#   name = "${var.name}-capacity-provider"
+resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
+  name = "${var.name}-capacity-provider"
 
-#   auto_scaling_group_provider {
-#     auto_scaling_group_arn = aws_autoscaling_group.ecs_asg.arn
+  auto_scaling_group_provider {
+    auto_scaling_group_arn = aws_autoscaling_group.ecs_asg.arn
 
-#     managed_scaling {
-#       maximum_scaling_step_size = 1000
-#       minimum_scaling_step_size = 1
-#       status                    = "ENABLED"
-#       target_capacity           = 1
-#     }
-#   }
-# }
+    managed_scaling {
+      maximum_scaling_step_size = 1000
+      minimum_scaling_step_size = 1
+      status                    = "ENABLED"
+      target_capacity           = 1
+    }
+  }
+}
 
 resource "aws_ecs_cluster_capacity_providers" "capacity_providers" {
   cluster_name = aws_ecs_cluster.ecs_cluster.name
